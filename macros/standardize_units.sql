b8e6d9f03943b30bbf245e6b9d3847dc44d62b76
Arsen Sarkisyan <s-arsen01@mail.ru>
2025-08-12 21:25:11 +0300

    fix: По замечаниям Юли был исправлен датасет нанозимов, убраны лишние столбцы, приведены значения к единому виду в столбце  vmax, и другие незначительные изменения

---
+++ macros/standardize_units.sql
@@ -0,0 +1,35 @@
+{% macro standardize_vmax(value_column, unit_column) %}
+    -- Этот макрос принимает имя колонки со значением и с юнитом
+    -- и возвращает ОДНО SQL-выражение для вычисления значения в M/s.
+
+    (
+        {{ value_column }} -- Берем исходное значение
+
+        -- Умножаем на коэффициент множителя (10^-x)
+        * case
+            when replace(replace(lower({{ unit_column }}), '−', '-'), ' ', '') like '%10-3%' then 1e-3
+            when replace(replace(lower({{ unit_column }}), '−', '-'), ' ', '') like '%10-4%' then 1e-4
+            when replace(replace(lower({{ unit_column }}), '−', '-'), ' ', '') like '%10-5%' then 1e-5
+            when replace(replace(lower({{ unit_column }}), '−', '-'), ' ', '') like '%10-6%' then 1e-6
+            when replace(replace(lower({{ unit_column }}), '−', '-'), ' ', '') like '%10-7%' then 1e-7
+            when replace(replace(lower({{ unit_column }}), '−', '-'), ' ', '') like '%10-8%' then 1e-8
+            when replace(replace(lower({{ unit_column }}), '−', '-'), ' ', '') like '%10-9%' then 1e-9
+            else 1.0
+        end
+
+        -- Умножаем на коэффициент концентрации
+        * case
+            when replace(replace(lower({{ unit_column }}), '−', '-'), ' ', '') like '%mm%' then 1e-3
+            -- ★★★ ИСПРАВЛЕНИЕ ЗДЕСЬ ★★★
+            when replace(replace(lower({{ unit_column }}), '−', '-'), ' ', '') like '%μm%' or replace(replace(lower({{ unit_column }}), '−', '-'), ' ', '') like '%um%' then 1e-6
+            when replace(replace(lower({{ unit_column }}), '−', '-'), ' ', '') like '%nm%' then 1e-9
+            else 1.0
+          end
+
+        -- Делим на коэффициент времени
+        / case
+            when replace(replace(lower({{ unit_column }}), '−', '-'), ' ', '') like '%min%' then 60.0
+            else 1.0
+          end
+    )
+{% endmacro %}
